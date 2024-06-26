8d98b88b1c4f1cc85f268f7c1d0a5c05525f5202
Jason Clark <jclark@maximusuk.co.uk>
2024-06-26 17:41:59 +0100

    trying other options to display flag value

--- src/webparts/testCamlQuery/TestCamlQueryWebPart.ts
+++ src/webparts/testCamlQuery/TestCamlQueryWebPart.ts
@@ -14,13 +14,18 @@ import { Web } from "@pnp/sp/webs";
 import "@pnp/sp/webs";
 import "@pnp/sp/lists";
 import { LogLevel, PnPLogging } from "@pnp/logging";
+import { SPComponentLoader } from '@microsoft/sp-loader';
+
+let libraryFlag : any[]=[];
+let libraryNameHTML : string = "";
+let libraryFlagHTML : string = "";
 
 export interface ITestCamlQueryWebPartProps {
   description: string;
   division : string;
   teamTermID : string;
   parentTermID : string;
-  libraryFlag: boolean[];
+  //libraryFlag: boolean[];
 }
 
 export default class TestCamlQueryWebPart extends BaseClientSideWebPart<ITestCamlQueryWebPartProps> {
@@ -30,7 +35,11 @@ export default class TestCamlQueryWebPart extends BaseClientSideWebPart<ITestCam
 
   public async render(): Promise<void> {
     const libraryName : string[] = ["Policies", "Procedures", "Guides", "Forms", "General"];
-    this.properties.libraryFlag = [];
+
+    for (let x = 0; x < libraryName.length; x++) {
+      libraryNameHTML += `<div>${libraryName[x]}</div>`;
+      await this.checkData(x,libraryName[x],"IPES Wales",""); //.then( ()=>{  
+    }
 
     this.domElement.innerHTML = `
     <section class="${styles.testCamlQuery} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
@@ -40,31 +49,21 @@ export default class TestCamlQueryWebPart extends BaseClientSideWebPart<ITestCam
         <div>${this._environmentMessage}</div>
         <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
       </div>
-      <div>
-        <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
+      <div class="row">     
+        <div class="col-4" id="libraryName"><h3>Library Names</h3></div>
+        <div class="col-4" id="libraryFlag"><h3>Library Flags</h3></div>
       </div>
     </section>`;
 
-    for (let x = 0; x < libraryName.length; x++) {
-      await this.checkData(x,libraryName[x],"IPES Wales",""); //.then( ()=>{  
-      console.log("library",libraryName[x],this.properties.libraryFlag[x]);      
+    //console.log("library",libraryName,libraryFlag);      
+    console.log(libraryNameHTML);
+
+    if(this.domElement.querySelector('#libraryName') != null) {
+      this.domElement.querySelector('#libraryName')!.innerHTML += libraryNameHTML;
     }
   }
   
-  private async checkData(num:number,library:string,team:string,category:string): Promise<any>{ //:Promise<ISPLists> {
+  public async checkData(num:number,library:string,team:string,category:string): Promise<void>{ //:Promise<ISPLists> {
     
     const sp = spfi().using(SPFx(this.context)).using(PnPLogging(LogLevel.Warning));  
     const tenant_uri = this.context.pageContext.web.absoluteUrl.split('/',3)[2];
@@ -115,18 +114,32 @@ export default class TestCamlQueryWebPart extends BaseClientSideWebPart<ITestCam
       await webDC.lists.getByTitle(library)
         .getItemsByCAMLQuery({ViewXml:view},"FieldValuesAsText/FileRef", "FieldValueAsText/FileLeafRef")
         .then(async (results) => {
-          console.log("check data",dcTitle," library",library,results);
+          //console.log("check data",dcTitle," library",library,results);
           if(results.length>0){   
-            console.log("response length",results.length);             
-            this.properties.libraryFlag[num]=true;
+            //console.log("response length",results.length);             
+            libraryFlag[num]=true;
+          } else{
+            libraryFlag[num]=false;
           }
+          console.log("libraryFlag",libraryFlag[num]);  
+          libraryFlagHTML += `<div>${libraryFlag[num]}</div>`;
         })
     });
+    console.log(libraryFlagHTML);
+
+    if(this.domElement.querySelector('#libraryFlag') != null) {
+      this.domElement.querySelector('#libraryFlag')!.innerHTML += libraryFlagHTML;
+    }
+    return;
   }
 
-  protected onInit(): Promise<void> {
+  public async onInit(): Promise<void> {
+    await super.onInit();
+    SPComponentLoader.loadCss("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css");
+    SPComponentLoader.loadCss("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css");
+
     return this._getEnvironmentMessage().then(message => {
-      this._environmentMessage = message;
+      //this._environmentMessage = message;
     });
   }
 
@@ -153,7 +166,6 @@ export default class TestCamlQueryWebPart extends BaseClientSideWebPart<ITestCam
           return environmentMessage;
         });
     }
-
     return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
   }
 
@@ -172,7 +184,6 @@ export default class TestCamlQueryWebPart extends BaseClientSideWebPart<ITestCam
       this.domElement.style.setProperty('--link', semanticColors.link || null);
       this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
     }
-
   }
 
   protected get dataVersion(): Version {
